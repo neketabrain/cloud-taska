e3d5709d5b55ee711bfadc7d65efa282a9a01cf1
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2022-06-09 01:11:07 +0700

    no tasks block

--- src/pages/home/table/index.tsx
+++ src/pages/home/table/index.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { taskLib, taskModel, TaskRow } from 'entities/task';
 import { Task } from 'features/task';
@@ -7,6 +8,8 @@ import { ROUTES } from 'shared/config';
 import styles from './styles.module.scss';
 
 export const TaskTable: React.FC = () => {
+  const { t } = useTranslation('task');
+
   const currentTaskId = taskModel.useCurrentTaskId();
   const tasks = taskModel.useFilteredTasks();
   const sortedTasks = useMemo(() => taskLib.sortTasksByDate(tasks), [tasks]);
@@ -19,16 +22,23 @@ export const TaskTable: React.FC = () => {
       </div>
 
       <ul className={styles.list}>
-        {sortedTasks.map((task) => (
-          <li key={task.id} className={styles.item}>
-            <TaskRow
-              task={task}
-              active={task.id === currentTaskId}
-              toggle={<Task.ToggleTaskMini task={task} />}
-              actions={[<Task.EditTask task={task} />, <Task.DeleteTask taskId={task.id} />]}
-            />
-          </li>
-        ))}
+        {!sortedTasks.length && (
+          <div className={styles.emptyContainer}>
+            <p className={styles.emptyText}>{t('noTasks')}</p>
+          </div>
+        )}
+
+        {!!sortedTasks.length &&
+          sortedTasks.map((task) => (
+            <li key={task.id} className={styles.item}>
+              <TaskRow
+                task={task}
+                active={task.id === currentTaskId}
+                toggle={<Task.ToggleTaskMini task={task} />}
+                actions={[<Task.EditTask task={task} />, <Task.DeleteTask taskId={task.id} />]}
+              />
+            </li>
+          ))}
       </ul>
     </section>
   );
--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { taskLib, taskModel, TaskRow } from 'entities/task';
 import { Task } from 'features/task';
@@ -6,6 +7,8 @@ import { Task } from 'features/task';
 import styles from './styles.module.scss';
 
 export const TasksPage: React.FC = () => {
+  const { t } = useTranslation('task');
+
   const currentTaskId = taskModel.useCurrentTaskId();
   const tasks = taskModel.useFilteredTasks();
   const sortedTasks = useMemo(() => taskLib.sortTasksByDate(tasks), [tasks]);
@@ -18,16 +21,23 @@ export const TasksPage: React.FC = () => {
 
       <section className={styles.tasks}>
         <ul className={styles.block}>
-          {sortedTasks.map((task) => (
-            <li key={task.id} className={styles.item}>
-              <TaskRow
-                task={task}
-                active={task.id === currentTaskId}
-                toggle={<Task.ToggleTaskMini task={task} />}
-                actions={[<Task.EditTask task={task} />, <Task.DeleteTask taskId={task.id} />]}
-              />
-            </li>
-          ))}
+          {!sortedTasks.length && (
+            <div className={styles.emptyContainer}>
+              <p className={styles.emptyText}>{t('noTasks')}</p>
+            </div>
+          )}
+
+          {!!sortedTasks.length &&
+            sortedTasks.map((task) => (
+              <li key={task.id} className={styles.item}>
+                <TaskRow
+                  task={task}
+                  active={task.id === currentTaskId}
+                  toggle={<Task.ToggleTaskMini task={task} />}
+                  actions={[<Task.EditTask task={task} />, <Task.DeleteTask taskId={task.id} />]}
+                />
+              </li>
+            ))}
         </ul>
       </section>
     </div>
