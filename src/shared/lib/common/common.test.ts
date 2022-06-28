edf80ae9f65633c5b4f5675a0b8426d3c96d0fb2
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2022-06-29 01:23:05 +0700

    add unit tests

--- src/shared/lib/common/common.test.ts
+++ src/shared/lib/common/common.test.ts
@@ -1,4 +1,4 @@
-import { hasValue } from './index';
+import { hasValue, getEnvVariable } from './index';
 
 describe('hasValue', () => {
   test('Should return true if value not undefined or not null', () => {
@@ -12,5 +12,16 @@ describe('hasValue', () => {
   test('Should return false if value is undefined or null', () => {
     expect(hasValue(undefined)).toBeFalsy();
     expect(hasValue(null)).toBeFalsy();
+  });
+});
+
+describe('getEnvVariable', () => {
+  test('Should throw error if env variable not set', () => {
+    expect(() => getEnvVariable('someVar')).toThrowError();
+  });
+
+  test('Should return value if it is set', () => {
+    process.env.someVar = 'test';
+    expect(getEnvVariable('someVar')).toBe('test');
   });
 });
